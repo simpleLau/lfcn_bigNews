c5bd1d7f570c98dd0d5d901d879ae5a2057ba4c2
simplelau <simple_lau@126.com>
2020-08-08 11:04:55 +0800

    添加文章列表渲染事件

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -1,8 +1,36 @@
 $(function () {
+    try { require('jQuery') } catch (err) { };
+    // 文章页面渲染
+    function articleInit(page, perpage, type, state) {
+        $.get({
+            url: BigNew.article_query,
+            dataType: 'json',
+            data: { page, perpage, type, state },
+            success(res) {
+                // console.log(res.data.data);
+                $('tbody').html(template('tbodyTp', res.data.data));
+            }
+        })
+    }
+    let page = 1;
+    let perpage = 10;
+    articleInit(page, perpage);
+
     $.get({
         url: BigNew.category_list,
+        dataType: 'json',
         success(res) {
-            console.log(res);
+            $('#selCategory').html(template('selectTp', res.data))
         }
     })
+
+
+    $('#btnSearch').click(function () {
+        console.log($('#selCategory').val());
+        let type = $('#selCategory').val();
+        let state = $('#selStatus').val();
+        articleInit(page, perpage, type, state);
+    })
+
+
 })
